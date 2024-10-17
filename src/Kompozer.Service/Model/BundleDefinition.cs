ac72f38fc2fa227ecf5d328e5c537e5e8748e552
martinstanek <stanekmartin@bluewin.ch>
2024-10-17 18:41:18 +0200

    test & definition

---
+++ src/Kompozer.Service/Model/BundleDefinition.cs
@@ -0,0 +1,32 @@
+﻿namespace Kompozer.Service.Model;
+
+public sealed record BundleDefinition
+{
+    public required Info Info { get; init; }
+
+    public required IReadOnlyCollection<Registry> Registries { get; init; }
+
+    public required IReadOnlyCollection<string> Images { get; init; }
+
+    public required IReadOnlyCollection<string> Stacks { get; init; }
+}
+
+public sealed record Info 
+{
+    public required string Name { get; init; }    
+
+    public required string Description { get; init; }
+
+    public required string Version { get; init; }
+}
+
+public sealed record Registry
+{
+    public required string Name { get; init; }
+
+    public required string Server { get; init; }
+
+    public string User { get; init; } = string.Empty;
+
+    public string Password { get; init; } = string.Empty;
+}
---
+++ tst/Kompozer.Tests/Service/Model/BundleDefinitionTests.cs
@@ -0,0 +1,19 @@
+﻿using Kompozer.Service.Model;
+using Shouldly;
+using System.Text.Json;
+using Xunit;
+
+namespace Kompozer.Tests.Service.Model;
+
+public sealed class BundleDefinitionTests
+{
+    [Fact]
+    public void Test()
+    {
+        var content = File.ReadAllText("Data/sample.json");
+
+        var definition = JsonSerializer.Deserialize<BundleDefinition>(content);
+
+        definition.ShouldNotBeNull().Info.Name.ShouldBe("TestBundle");
+    }
+}
