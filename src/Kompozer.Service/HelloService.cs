33613a7ec3efdefdd9be87a6dc90cbcb8c94ba1c
martinstanek <stanekmartin@bluewin.ch>
2024-10-17 22:20:23 +0200

    json

--- src/Kompozer.Service/HelloService.cs
+++ src/Kompozer.Service/HelloService.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Kompozer.Service.Model;
+using Kompozer.Service.Serialization;
 using Microsoft.Extensions.Hosting;
 
 namespace Kompozer.Service;
@@ -14,13 +17,40 @@ public sealed class HelloService : BackgroundService
         Console.WriteLine("Scanning for definitions ..");
 
         var workDir = AppContext.BaseDirectory;
-        var files = Directory.GetFiles(workDir).Where(f => f.EndsWith("json", StringComparison.OrdinalIgnoreCase));
+        var files = Directory.GetFiles(workDir)
+            .Where(f => f.EndsWith("json", StringComparison.OrdinalIgnoreCase));
 
         foreach (var file in files)
         {
             Console.WriteLine(file);
+
+            if (TryParseBundleDefinition(file, out var definition) && definition is not null)
+            {
+                Console.WriteLine(definition.Info.Name);
+                Console.WriteLine(definition.Info.Description);
+            }
         }
 
+
         return Task.CompletedTask;
+    }
+
+    private static bool TryParseBundleDefinition(string path, out BundleDefinition? definition)
+    {
+        var content = File.ReadAllText(path);
+        definition = default;
+
+        try
+        {
+            definition = JsonSerializer.Deserialize(content, SourceGenerationContext.Default.BundleDefinition);
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            
+            return false;
+        }
     }
 }
---
+++ src/Kompozer.Service/Serialization/SourceGenerationContext.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.Json.Serialization;
+using Kompozer.Service.Model;
+
+namespace Kompozer.Service.Serialization;
+
+[JsonSerializable(typeof(BundleDefinition), GenerationMode = JsonSourceGenerationMode.Default)]
+internal partial class SourceGenerationContext : JsonSerializerContext;
